f0e86a676f881d6d24172884fdb423bec672bed8
Raul Castillo <sx353553@yahoo.com>
2026-06-04 09:39:02 -0500

    update code

--- books.js
+++ books.js
@@ -1,4 +1,23 @@
-
+function renderBooks() {
+  <div class="book">
+    <figure class="book__img--wrapper">
+    <img class="book__img" src="assets/deep work.jpeg" alt="">
+    </figure>
+    <div class="book__title">
+     Deep Work
+    </div>
+    <div class="book__ratings">
+     <i class="fas fa-star"></i>
+     <i class="fas fa-star"></i>
+     <i class="fas fa-star"></i>
+     <i class="fas fa-star"></i>
+     <i class="fas fa-star-half-alt"></i>
+     </div>
+     <div class="book__price">
+     <span class="book__price--normal">$59.95</span> $14.95
+     </div>
+     </div>
+}
 
 // FAKE DATA
 function getBooks() {
