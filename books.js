c819bbd4fdaa7d5c586def4c038f39331ca96c73
Raul Castillo <sx353553@yahoo.com>
2026-06-09 06:35:19 -0500

    update code

--- books.js
+++ books.js
@@ -1,25 +1,37 @@
 function renderBooks() {
   const booksWrapper = document.querySelector('.books');
-  console. log(booksWrapper);
- // `<div class="book">
- //  <figure class="book__img--wrapper">
- // <img class="book__img" src="assets/deep work.jpeg" alt="">
- // </figure>
- // <div class="book__title">
- // Deep Work
- //  </div>
- // <div class="book__ratings">
- //     <i class="fas fa-star"></i>
- //    <i class="fas fa-star"></i>
- //    <i class="fas fa-star"></i>
- //  <i class="fas fa-star"></i>
- // <i class="fas fa-star-half-alt"></i>
- // </div>
- // <div class="book__price">
- // <span class="book__price--normal">$59.95</span> $14.95
- // </div>
- // </div>`
+
+
+const books = getBooks();
+const booksHtml = books.map((book) => {
+ return `<div class="book">
+   <figure class="book__img--wrapper">
+  <img class="book__img" src="${book.url}" alt="">
+  </figure>
+  <div class="book__title">
+  ${book.title}
+  </div>
+  <div class="book__ratings">
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star"></i>
+  <i class="fas fa-star-half-alt"></i>
+  </div>
+  <div class="book__price">
+  <span class="book__price--normal">${books[0].originalPrice}</span> $${books[0].salePrice}
+  </div>
+  </div>`
+})
+.join('');
+
+console.log(booksHtml);
+
+  booksWrapper.innerHTML = booksHtml;
+  console.log(booksHtml);
+  // booksWrapper.innerHTML =;
 }
+
 
 setTimeout(( ) => {
   renderBooks();
